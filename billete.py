b353d31a9ecbcab7456745828323f805ca6d1069
albertodt <delgadotoralalberto@gmail.com>
2025-09-18 13:02:27 +0200

    Update: Billete y Usuario

--- billete.py
+++ billete.py
@@ -1,9 +1,16 @@
 class Billete:
     __contador_billetes = 0
     __precio = 100
+    
     def __init__(self):
-        self.__id = self.__contador_billetes
-        self.__contador_billetes += 1
+        Billete.__contador_billetes += 1
+        self.__id = Billete.__contador_billetes
         
-    def get_billetes(self):
+    def get_billete(self):
         return f"ID: {self.__id}, Precio: {self.__precio} pesos"
+    
+    def get_id(self):
+        return self.__id
+    
+    def get_precio(self):
+        return self.__precio
--- usuario.py
+++ usuario.py
@@ -1,15 +1,32 @@
 class Usuario:
+    __contador_usuarios = 0
+    
     def __init__(self, nombre, apellido):
+        Usuario.__contador_usuarios += 1
+        self.__id = Usuario.__contador_usuarios
         self.__nombre = nombre
         self.__apellido = apellido
-        self.billetes = []
+        self.__billetes = []
 
-    def editar_usuario(self, nombre, apellido):
-        self.__nombre = nombre
-        self.__apellido = apellido
+    def get_id(self):
+        return self.__id
+
+    def editar_usuario(self, nombre=None, apellido=None):
+        if nombre is not None:
+            self.__nombre = nombre
+        if apellido is not None:
+            self.__apellido = apellido
 
     def get_usuario(self):
-        return f"Nombre: {self.__nombre}, Apellido: {self.__apellido}"
+        return f"ID: {self.__id}, Nombre: {self.__nombre}, Apellido: {self.__apellido}"
     
     def get_billetes(self):
-        return self.billetes
+        return self.__billetes.copy()
+    
+    def agregar_billete(self, billete):
+        self.__billetes.append(billete)
+    
+    def remover_billetes(self, billetes):
+        for billete in billetes:
+            if billete in self.__billetes:
+                self.__billetes.remove(billete)
