f8db5f9436124a91aea2bd37c3ca9aea3b4b59b5
Alvaro Silva <102283382+BabyScylla69@users.noreply.github.com>
2025-09-19 09:38:28 +0200

    fixed flotaBuses variable accessibility

--- flotaBuses.py
+++ flotaBuses.py
@@ -1,13 +1,13 @@
 from bus import Bus
 
 class FlotaBus:
-    flotaBuses = []
+    __flotaBuses = []
     
     def __init_(self):
         pass
     def get_bus_by_id(self, id):
-        return self.flotaBuses[id]
+        return self.__flotaBuses[id]
     def get_buses(self):
-        return self.flotaBuses
+        return self.__flotaBuses
     def add_bus(self, bus):
-        self.flotaBuses.append(bus)
+        self.__flotaBuses.append(bus)
